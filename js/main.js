4cf17b66f53a9631f3b5b6b4b480cf90dc540915
Katerina Romanova <e.romanova@naveksoft.com>
2022-03-16 23:04:49 +0300

    bugfix

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
 
 function getRandomNumber(minValue, maxValue) {
   if(minValue < 0 || maxValue < 0 || minValue >= maxValue){
-    console.log('Диапозон введен неверно');
+    return false;
   }
-  let rand = minValue - 0.5 + Math.random() * (maxValue - minValue + 1);
+  const rand = minValue - 0.5 + Math.random() * (maxValue - minValue + 1);
   return Math.round(rand);
 }
 
 function getRandomFloat(minValue, maxValue, numberAfterComa) {
   if(minValue < 0 || maxValue < 0 || minValue >= maxValue){
-    console.log('Диапозон введен неверно');
+    return false;
   }
-  let rand = Math.random() * (maxValue - minValue) + minValue;
+  const rand = Math.random() * (maxValue - minValue) + minValue;
   return rand.toFixed(numberAfterComa);
 }
